3f6a492a36689646cf964cb48ec962db62b8c00d
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-06-06 13:03:36 -0400

    lol

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-const NavBar = () =>{
-    return(
-        <ul className="nav">
-            <li>
-                <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-                <NavLink to="/register">Sign Up</NavLink>
-            </li>
-            <li>
-                <NavLink to="/communities">Communities</NavLink>
-            </li>
-        </ul>
-    )
-};
+const NavBar = () => {
+  return (
+    <ul className='nav'>
+      <li>
+        <NavLink to='/'>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to='/login'>Login</NavLink>
+      </li>
+      <li>
+        <NavLink to='/register'>Sign Up</NavLink>
+      </li>
+      <li>
+        <NavLink to='/communities'>Communities</NavLink>
+      </li>
+    </ul>
+  )
+}
 
-export default NavBar;
+export default NavBar
--- src/containers/ComContainer.jsx
+++ src/containers/ComContainer.jsx
@@ -2,30 +2,26 @@ import React, { Component } from 'react'
 import Community from '../components/Community'
 
 export class ComContainer extends Component {
+  render () {
+    const arrayOfComponents = this.props.communities.map((singleCom) => {
+      return <Community
+        key={singleCom.id}
+        community={singleCom}
+        addNewArtwork={this.props.addNewArtwork}
+        deleteArtwork={this.props.deleteArtwork}
+        user={this.props.user}
+             />
+    })
 
+    return (
+      <div className='CommunityContainer'>
+        {arrayOfComponents.map((singleComp) => {
+          return <li className='SingleCommunity'> {singleComp}</li>
+        })}
 
-    
-
-    render() {
-        let arrayOfComponents = this.props.communities.map((singleCom)=>{
-            return <Community
-            key={singleCom.id}
-            community={singleCom}
-            addNewArtwork={this.props.addNewArtwork}
-            deleteArtwork={this.props.deleteArtwork}
-            user={this.props.user}
-            />
-        })
-        
-        return (
-            <div className="CommunityContainer">
-                {arrayOfComponents.map((singleComp)=>{
-                    return <li className="SingleCommunity"> {singleComp}</li>
-                })}
-                
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default ComContainer
